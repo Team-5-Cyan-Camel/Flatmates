232ac42a36d35f2be6b8822c746f2cea98df2116
Wong <wcho400@aucklanduni.ac.nz>
2021-04-10 12:16:07 +1200

    Added basic frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Link,
+} from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  // let [userObj, ]
+
   return (
     <Router>
       {/* path for main page */}
       <Route path="/" exact>
         <p>Main</p>
-        <Link to="/signup">
-          signup
-        </Link>
+        <Link to="/signup">Signup</Link>
       </Route>
 
       {/* path for sign up screen */}
@@ -17,20 +23,19 @@ function App() {
         <p>signup</p>
       </Route>
 
-      {/* path for code screen */}
-        <Route path="/code">
-      <p>code</p>
+      {/* path for room code to give */}
+      <Route path="/code">
+        <p>code</p>
       </Route>
-
 
       {/* path for room screen */}
       <Route path="/room">
-      <p>room</p>
+        <p>room</p>
       </Route>
 
       {/* path for incompatable path */}
       <Route path="*">
-        <Redirect to ="/"/>
+        <Redirect to="/" />
       </Route>
     </Router>
   );
