8623445b22083e5b22dab4bad8ee9d61e4c0ecca
JChamberlain28 <JChamberlain28@outlook.com>
2021-04-12 14:12:54 +1200

    added endpoints for room operations

---
+++ backend/routes/room.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+
+/* POST a room to be created */
+router.post('/', function(req, res, next) {
+    const responseBody = {RoomCode : "XXXX"}
+    res.status(201).send(responseBody)
+});
+
+
+/* POST a request to join a room */
+router.post('/join', function(req, res, next) {
+    res.status(201).send();
+});
+
+/* PACH room details and remove the user from the room */
+router.patch('/leave', function(req, res, next) {
+    res.status(200).send();
+});
+
+
+/* GET all information for a room */
+router.get('/', function(req, res, next) {
+    // TODO: representation of user, reminder, expenses, roster is for illustration only currently
+    const responseBody = {Code: "XXXX", Users: ["User1", "User2", "User3"], Reminders: ["R1", "R2"], Expenses: [], Roster: []};
+    res.status(200).send(responseBody);
+});
+
+
+/* DELETE a room */
+router.delete('/', function(req, res, next) {
+    // check the user is the room owner
+    // delete the room
+    res.status(200)
+});
+    
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
