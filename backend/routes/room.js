eb614b7dc475f7c7cdf3472b7a31d5c19e7adce4
JChamberlain28 <JChamberlain28@outlook.com>
2021-04-12 14:41:26 +1200

    changed res.send() to res.json()

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -4,18 +4,18 @@ var router = express.Router();
 /* POST a room to be created */
 router.post('/', function(req, res, next) {
     const responseBody = {RoomCode : "XXXX"}
-    res.status(201).send(responseBody)
+    res.status(201).json(responseBody)
 });
 
 
 /* POST a request to join a room */
 router.post('/join', function(req, res, next) {
-    res.status(201).send();
+    res.status(201);
 });
 
 /* PACH room details and remove the user from the room */
 router.patch('/leave', function(req, res, next) {
-    res.status(200).send();
+    res.status(200);
 });
 
 
@@ -23,7 +23,7 @@ router.patch('/leave', function(req, res, next) {
 router.get('/', function(req, res, next) {
     // TODO: representation of user, reminder, expenses, roster is for illustration only currently
     const responseBody = {Code: "XXXX", Users: ["User1", "User2", "User3"], Reminders: ["R1", "R2"], Expenses: [], Roster: []};
-    res.status(200).send(responseBody);
+    res.status(200).json(responseBody);
 });
 
 
