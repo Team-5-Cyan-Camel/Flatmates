3b2c689a6c10278d87ea0656eefff6f4b774a27a
UltimaKuma <ChinL@outlook.co.nz>
2021-04-25 11:40:16 +1200

    Added error handler for mongoose and ran formatter

--- backend/app.js
+++ backend/app.js
@@ -1,49 +1,61 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-require('dotenv').config({path: __dirname + '/.env'})
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+require("dotenv").config({ path: __dirname + "/.env" });
 
-var indexRouter = require('./routes/index');
-var roomRouter = require('./routes/room');
-var userRouter = require('./routes/user');
-var rosterRouter = require('./routes/roster');
+var indexRouter = require("./routes/index");
+var roomRouter = require("./routes/room");
+var userRouter = require("./routes/user");
+var rosterRouter = require("./routes/roster");
 
 var app = express();
 
-mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@flatmates-database-dev.lwm8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+var mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@flatmates-database-dev.lwm8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+mongoose.connect(
+  mongoUri,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("Successfully connected to MongoDB Atlas.");
+    }
+  }
+);
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/room', roomRouter);
-app.use('/user', userRouter);
-app.use('/roster', rosterRouter);
+app.use("/", indexRouter);
+app.use("/room", roomRouter);
+app.use("/user", userRouter);
+app.use("/roster", rosterRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
