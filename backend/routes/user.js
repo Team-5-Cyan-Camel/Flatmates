d0c2271878421718043ebad96dcad37989f9d85e
UltimaKuma <ChinL@outlook.co.nz>
2021-04-25 17:30:33 +1200

    Added cookie implementation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var User = require('../mongo/User');
+var uuid = require('uuid')
 
 /* POST user register. */
 router.post("/register", async function (req, res, next) {
@@ -9,16 +10,34 @@ router.post("/register", async function (req, res, next) {
     password: req.body.password,
     contactDetails: req.body.contactDetails,
     name: req.body.name,
-    phoneNumber: req.body.phoneNumber,
-    sessionID: req.body.sessionID,
+    phoneNumber: req.body.phoneNumber
   });
 
+  let user;
   try {
-    const user = await newUser.save();
-    res.status(200).json(user);
+    user = await newUser.save();
   } catch (error) {
-    res.status(500).json({message: error.message});
+    res.status(500).json({ message: error.message });
   }
+
+  // Creating and setting new cookie
+  let cookie = req.cookies.cookieName;
+  if (cookie === undefined) {
+    let sessionID = uuid.v4()
+    // Setting for frontend response
+    res.cookie('sessionID', sessionID, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
+    // Setting for database
+    try {
+      await User.updateOne(
+          {_id: user._id},
+          {$set: {sessionID: sessionID}},
+      );
+    } catch (error) {
+      res.status(500).json({message: error.message});
+    }  
+  }
+
+  res.status(200).json(user);
 });
 
 /* POST user login. */
