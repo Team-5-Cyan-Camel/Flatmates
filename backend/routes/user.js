4ee8c4f85cf97286610d7e8085022ca7b0983715
UltimaKuma <ChinL@outlook.co.nz>
2021-04-25 16:49:05 +1200

    Removed unsettable fields in user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,26 +4,14 @@ var User = require('../mongo/User');
 
 /* POST user register. */
 router.post("/register", async function (req, res, next) {
-  console.log(req.body.socketID)
   const newUser = new User({
-    socketID: req.body.socketID,
     username: req.body.username,
     password: req.body.password,
-    roomCode: req.body.roomCode,
-    isHost: req.body.isHost,
     contactDetails: req.body.contactDetails,
     name: req.body.name,
     phoneNumber: req.body.phoneNumber,
     sessionID: req.body.sessionID,
   });
-
-  // newUser.save((err) => {
-  //   if (err) {
-  //     console.log(err);
-  //     return res.status(400).json({ error: err });
-  //   }
-  //   return res.status(201).json(newUser);
-  // });
 
   try {
     const user = await newUser.save();
@@ -31,8 +19,6 @@ router.post("/register", async function (req, res, next) {
   } catch (error) {
     res.status(500).json({message: error.message});
   }
-
-  
 });
 
 /* POST user login. */
