4ad2fd314182b2d2b7b1bdbfde0d6df33ed67a60
UltimaKuma <ChinL@outlook.co.nz>
2021-04-26 15:21:39 +1200

    Fixed problems with interaction between jest and mongoDB. Sleep needed to be used as cannot await in top-level.

--- backend/test/index.test.js
+++ backend/test/index.test.js
@@ -1,14 +1,20 @@
 const request = require("supertest");
 const app = require("../app");
 
+beforeAll(async () => {
+  await sleep(2000)
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 describe("Test the root path", () => {
-  test("It should response the GET method", done => {
-    request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toBe('This is the index for the Flatmates server');
-        done();
-      });
+  test("It should response the GET method", async done => {
+    const res = await request(app).get("/")
+
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe('This is the index for the Flatmates server');
+    done();
   });
 });
