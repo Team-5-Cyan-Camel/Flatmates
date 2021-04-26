efa8cda99192d1c0e81ff14be204c0cc7db9309a
UltimaKuma <ChinL@outlook.co.nz>
2021-04-27 10:13:18 +1200

    WIP roster endpoint

--- backend/routes/roster.js
+++ backend/routes/roster.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
+var User = require('../mongo/User');
+var Room = require('../mongo/Room');
+
 
 /* POST new roster. */
-router.post("/", function (req, res, next) {
+router.post("/", getUser, getRoom, function (req, res, next) {
+  console.log(req.user)
   res.status(201).json({ rosterID: "jkasd" });
 });
 
@@ -25,5 +29,26 @@ router.delete("/task", function (req, res, next) {
 router.delete("/task", function (req, res, next) {
   res.status(200).send("task successfully deleted");
 });
+
+async function getUser(req, res, next) {
+  req.user = await User.findOne({ sessionID: req.cookies.sessionID });
+  if (!req.user) {
+    return res.status(401).json({ message: "Invalid cookies" })
+  } else {
+    next();
+  }
+}
+
+async function getRoom(req, res, next) {
+  if (!req.user.roomCode) {
+    return res.status(403).json({ message: "Not in a room" })
+  }
+  req.room = await Room.findOne({ _id: req.user.roomCode });
+  if (!req.room) {
+    res.status(404).json({ message: "Room not found" })
+  } else {
+    next();
+  }
+}
 
 module.exports = router;
