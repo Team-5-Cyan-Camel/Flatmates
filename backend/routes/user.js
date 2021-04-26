4d3edaae9841603ccea98f0b45fa134d7062f5df
UltimaKuma <ChinL@outlook.co.nz>
2021-04-26 13:46:21 +1200

    Created setSessionCookie function and added login endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -13,36 +13,65 @@ router.post("/register", async function (req, res, next) {
     phoneNumber: req.body.phoneNumber
   });
 
+  // Saving user and setting cookies
   let user;
   try {
     user = await newUser.save();
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+    await setSessionCookie(req, res, user._id);
+    return res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
   }
-
-  // Creating and setting new cookie
-  let cookie = req.cookies.cookieName;
-  if (cookie === undefined) {
-    let sessionID = uuid.v4()
-    // Setting for frontend response
-    res.cookie('sessionID', sessionID, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
-    // Setting for database
-    try {
-      await User.updateOne(
-          {_id: user._id},
-          {$set: {sessionID: sessionID}},
-      );
-    } catch (error) {
-      res.status(500).json({message: error.message});
-    }  
-  }
-
-  res.status(200).json(user);
 });
 
 /* POST user login. */
-router.post("/login", function (req, res, next) {
-  res.status(200).send("user logged in");
+router.post("/login", async function (req, res, next) {
+  // Obtain user
+  let user;
+  try{
+    user = await User.findOne({
+      username: req.body.username,
+      password: req.body.password
+    })
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  
+  // If user does not exist, credentials were incorrect
+  if(user === null) {
+    return res.status(403).json({ message: "Incorrect username or password" });
+  }
+
+  // Setting cookies
+  try {
+    await setSessionCookie(req, res, user._id);
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  // Creating response
+  return res.status(200).json({
+    inRoom: user.roomCode ? true : false,
+    roomCode: user.roomCode
+  });
 });
+
+async function setSessionCookie(req, res, id) {
+  return new Promise(async (resolve) => {
+    // Creating and setting new cookie
+    let cookie = req.cookies.sessionID;
+    if (cookie === undefined) {
+      let newCookie = uuid.v4()
+      // Setting for frontend response
+      res.cookie('sessionID', newCookie, { maxAge: 7 * 24 * 60 * 60 * 1000, httpOnly: true });
+      // Setting for database
+      await User.updateOne(
+        { _id: id },
+        { $set: { sessionID: newCookie } },
+      );
+    }
+    resolve();
+  });
+}
 
 module.exports = router;
