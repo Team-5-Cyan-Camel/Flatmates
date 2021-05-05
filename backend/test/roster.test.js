1927f1f0be148e7e6ea243fdfc564abea5117a2f
UltimaKuma <ChinL@outlook.co.nz>
2021-05-05 23:15:48 +1200

    Fixed test changes to respone

--- backend/test/roster.test.js
+++ backend/test/roster.test.js
@@ -19,7 +19,7 @@ describe("Tests the endpoints dealing with roster", () => {
         expect(postRes.statusCode).toBe(200);
 
         const delReq = {
-            rosterID: postRes.body.rosters[postRes.body.rosters.length-1]._id
+            rosterID: postRes.body[postRes.body.length-1]._id
         }
         const delRes = await request(app).delete("/roster").set('Cookie', ['sessionID=1']).send(delReq).set('Accept', 'application/json')
 
@@ -51,7 +51,7 @@ describe("Tests the endpoints dealing with roster", () => {
 
         const delReq = {
             rosterID: "608e26d28b440022408c0b84",
-            taskID: postRes.body.rosters[0].tasks[postRes.body.rosters[0].tasks.length -1]
+            taskID: postRes.body[0].tasks[postRes.body[0].tasks.length -1]
         }
         const delRes = await request(app).delete("/roster/task").set('Cookie', ['sessionID=1']).send(delReq).set('Accept', 'application/json')
 
