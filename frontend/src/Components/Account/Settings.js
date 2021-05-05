c997d1728e2cfc1354cb8c5638c34b805ae72626
Wong <wcho400@aucklanduni.ac.nz>
2021-05-05 19:12:49 +1200

    Refactore settings page to actuall show content

--- frontend/src/Components/Account/Settings.js
+++ frontend/src/Components/Account/Settings.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom";
 import { FaTimes as Cross } from "react-icons/fa";
 import styles from "./Settings.module.css";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const modalRoot = document.querySelector("#modal-root");
 
@@ -8,8 +10,33 @@ const Settings = ({ hideSettings }) => {
   return ReactDOM.createPortal(
     <div className={styles.modalContainer}>
       {/* add code */}
-      <Cross onClick={hideSettings} />
-      <p>TESTTT</p>
+      <Card
+        id="Card-field"
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Card.Header
+          as="h5"
+          id="Card-Header"
+          className="text-center"
+          style={{ width: "100%" }}
+        >
+          {" "}
+          Settings
+          <Cross onClick={hideSettings} />
+        </Card.Header>
+
+        <Card.Body
+          style={{
+            display: "Grid",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "90%",
+          }}
+        ></Card.Body>
+      </Card>
     </div>,
     modalRoot
   );
---
+++ frontend/src/Components/Roster/AddTask.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const modalRoot = document.querySelector("#modal-root");
+
+const AddTask = () => {
+  return (<div></div>), modalRoot;
+};
+
+export default AddTask;
