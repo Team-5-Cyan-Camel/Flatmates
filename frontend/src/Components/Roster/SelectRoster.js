61646b0cd0c8f5ad4a3084cbcef4db298b29501b
OverCry <54381704+OverCry@users.noreply.github.com>
2021-05-06 09:59:22 +1200

    [FEAT] Roster Template (#18)
    * import jest
    
    * Refactor buttons to fit styling
    
    * fix buttons
    
    * Create roster elements
    
    * Roster nav bar made
    
    * Fixed edge case for zero rosters
    
    * Filtered tasks to associated personal
    
    * Tasks are shown for each user
    
    * roster finished
    
    * Refactore settings page to actuall show content
    
    * Settings modal is shown
    
    * Moved code for make task down
    
    * Created settings dummy modal
    
    * Temp display for adding task
    
    * Added styling for adding task
    
    * Renamed files for higher usability
    
    * Created template for adding roster
    
    * Made dummy method for adding yourself
    
    * Fixed settings styling

---
+++ frontend/src/Components/Roster/SelectRoster.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import "../Lobby/NavBar.css";
+import AddRoster from "./AddRoster";
+import { useState } from "react";
+
+const SelectRoster = ({ rosters, setRoster }) => {
+  let [makeRoster, setMakeRoster] = useState(false);
+
+  return (
+    <>
+      {/* {rosters !== "undefined" && ( */}
+      <Card
+        id="Card-field"
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Card.Body
+          style={{
+            display: "Flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "90%",
+          }}
+        >
+          {/* <div class='topnav' style={{display: 'flex'}}> */}
+          {rosters !== "undefined" &&
+            rosters.map((e) => [
+              <Button className="GoButton" onClick={() => setRoster(e.title)}>
+                {e.title}
+              </Button>,
+            ])}
+          <Button className="GoButton" onClick={() => setMakeRoster(true)}>
+            Add+
+          </Button>
+          {/* </div> */}
+        </Card.Body>
+      </Card>
+      {/* )} */}
+      {makeRoster && <AddRoster show={setMakeRoster} />}
+    </>
+  );
+};
+
+export default SelectRoster;
