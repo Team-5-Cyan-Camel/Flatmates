24a1df6297274cc622066598d76338d5cf9ffbe9
OverCry <54381704+OverCry@users.noreply.github.com>
2021-05-06 20:29:56 +1200

    [INIT] Room Codes #23 (#26)
    * import jest
    
    * Added login functionality
    
    * Changes were not saved
    
    * Room code generation works
    
    * Allow room joining
    
    * Remove non working alert

--- frontend/src/Components/Code/GenerateRoom.js
+++ frontend/src/Components/Code/GenerateRoom.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
+import axios from "axios";
 
 const GenerateRoom = () => {
   const history = useHistory();
@@ -9,20 +10,22 @@ const GenerateRoom = () => {
     // redirects to the room with the code
     console.log("generate room");
 
-    // at api call, go to room
-    // if (doesnotexist) {
-    //   return <Redirect to="/room/TEST" />;
-    // }
-    history.push("/room/TEMP");
+    axios
+      .post("room")
+      .then((res) => {
+        console.log(res);
+        history.push("/room/" + res.data.roomCode);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
 
   return (
     <>
-                  <Button
-                    onClick={MakeRoom}
-                    className='GoButton'
-
-                  >Generate</Button>
+      <Button onClick={MakeRoom} className="GoButton">
+        Generate
+      </Button>
     </>
   );
 };
