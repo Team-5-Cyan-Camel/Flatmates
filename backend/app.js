58638c296bdb6438b8680a225cb33c7a17b67d18
Leon <ChinL@outlook.co.nz>
2021-05-06 14:35:33 +1200

    Removed uri log

--- backend/app.js
+++ backend/app.js
@@ -22,8 +22,6 @@ if(process.env.NODE_ENV==="test"){
 
 var mongoUri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@flatmates-database-dev.lwm8u.mongodb.net/${mongodbDatabase}?retryWrites=true&w=majority`;
 
-console.log("URI",mongoUri.split('').join(' '));
-
 mongoose.connect(
   mongoUri,
   { useNewUrlParser: true, useUnifiedTopology: true },
