90b15d98feb93d6a55cd75bf5b85ef35eaff5bc2
UltimaKuma <ChinL@outlook.co.nz>
2021-05-09 00:02:13 +1200

    Added extra messages for joining and leaving

--- backend/sockets/ConnectionEvents.js
+++ backend/sockets/ConnectionEvents.js
@@ -3,22 +3,38 @@ module.exports = function (io) {
         socket.on('enter_room', (req) => {
             socket.join(req.roomID);
             console.log("A user joined", req.roomID);
+            let message = "A user joined"
+            emitMessageToSocketRoom(io, socket, message)
         });
+
+        socket.on('message', (req) => {
+            let message = `${req.username}: ${req.message}`
+            emitMessageToSocketRoom(io, socket, message)
+        });
+
 
         socket.on('leave_room', (req) => {
             socket.leave(req.roomID);
             console.log("A user left", req.roomID);
+            let message = "A user left"
+            emitMessageToSocketRoom(io, socket, message)
         });
 
-        socket.on('message', (req) => {
-            // Remove room if it is the socket id itself
-            let rooms = Array.from(socket.rooms).filter(function(item) {
-                return item !== socket.id;
-            });
-            console.log("message sent to",rooms)
-            if (rooms && rooms.length) {
-                io.in(rooms[0]).emit('message_update', `${req.username}: ${req.message}`);
-            }
+
+        socket.on('disconnecting', (req) => {
+            let message = "A user left"
+            emitMessageToSocketRoom(io, socket, message)
         });
     });
+}
+
+function emitMessageToSocketRoom(io, socket, message) {
+    // Remove room if it is the socket id itself
+    let rooms = Array.from(socket.rooms).filter(function (item) {
+        return item !== socket.id;
+    });
+    console.log("message sent to", rooms[0])
+    if (rooms && rooms.length) {
+        io.in(rooms[0]).emit('message_update', message);
+    }
 }
