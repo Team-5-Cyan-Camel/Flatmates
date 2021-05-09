26d559d4d811fa9333771225d4271fa835521890
OverCry <wcho400@aucklanduni.ac.nz>
2021-05-09 13:52:06 +1200

    Added alert for deleting a roster

--- frontend/src/Components/Roster/SelectRoster.js
+++ frontend/src/Components/Roster/SelectRoster.js
@@ -1,55 +1,71 @@
-import React from "react";
-import Card from "react-bootstrap/Card";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Button from "react-bootstrap/Button";
-import "../Lobby/NavBar.css";
-import AddRoster from "./AddRoster";
-import { useState, useEffect } from "react";
-import { FaTimes as Cross } from "react-icons/fa";
-import axios from "axios";
-import "../Lobby/NavBar.css";
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import '../Lobby/NavBar.css';
+import AddRoster from './AddRoster';
+import {useState, useEffect} from 'react';
+import {FaTimes as Cross} from 'react-icons/fa';
+import axios from 'axios';
+import '../Lobby/NavBar.css';
 
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { Link, NavLink, useHistory, useParams } from "react-router-dom";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import {Link, NavLink, useHistory, useParams} from 'react-router-dom';
+import {confirmAlert} from 'react-confirm-alert'; // Import
+import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
 
-const SelectRoster = ({ rosters, setRoster, updateDb, isHost }) => {
+const SelectRoster = ({rosters, setRoster, updateDb, isHost}) => {
   let [makeRoster, setMakeRoster] = useState(false);
 
   const deleteRoster = (id) => {
-    const rostDel = {
-      rosterID: id,
-    };
+    confirmAlert({
+      title: 'Deleting Roster',
+      message: 'Are you sure you want to delete this roster?',
+      buttons: [
+        {
+          label: 'Ok',
+          onClick: () => {
+            const rostDel = {
+              rosterID: id,
+            };
 
-    axios
-      .delete("/roster", { data: rostDel })
-      .then((res) => {})
-      .catch(function (error) {
-        console.log(error);
-      });
+            axios
+              .delete('/roster', {data: rostDel})
+              .then((res) => {})
+              .catch(function (error) {
+                console.log(error);
+              });
+          },
+        },
+        {
+          label: 'Cancel',
+        },
+      ],
+    });
   };
 
   return (
     <>
       {/* {rosters !== "undefined" && ( */}
 
-      <Navbar bg="dark" variant="dark" style={{}}>
-        <Nav className="mr-auto">
+      <Navbar bg='dark' variant='dark' style={{}}>
+        <Nav className='mr-auto'>
           {rosters !== null &&
             rosters.rosters.map((e, i) => [
               <Nav.Link
                 key={i}
                 onClick={() => setRoster(e.title)}
-                style={{ outline: "1px solid black ", outlineColor: "black" }}
+                style={{outline: '1px solid black ', outlineColor: 'black'}}
               >
-                {e.title}{" "}
+                {e.title}{' '}
                 {isHost && <Cross onClick={() => deleteRoster(e._id)} />}
               </Nav.Link>,
             ])}
 
           {/* </div> */}
         </Nav>
-        <Button className="NavBarButton" onClick={() => setMakeRoster(true)}>
+        <Button className='NavBarButton' onClick={() => setMakeRoster(true)}>
           add
         </Button>
       </Navbar>
