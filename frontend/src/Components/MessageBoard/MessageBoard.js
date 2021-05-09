c093dd3a77a93cbbb2822a9703b7e84ae31afa43
Leon <ChinL@outlook.co.nz>
2021-05-09 13:30:32 +1200

    Added username to messageboard

--- frontend/src/Components/MessageBoard/MessageBoard.js
+++ frontend/src/Components/MessageBoard/MessageBoard.js
@@ -1,73 +1,88 @@
-import { React, useEffect, useState, useRef, useContext } from 'react';
+import { React, useEffect, useState, useRef, useContext } from "react";
 import Card from "react-bootstrap/Card";
-import ListGroup from 'react-bootstrap/ListGroup';
-import FormControl from 'react-bootstrap/FormControl';
+import ListGroup from "react-bootstrap/ListGroup";
+import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import { SocketContext } from "../../Context/socketContext";
+import axios from "axios";
+import { FaSkating } from "react-icons/fa";
 
-const Chatbox = ({messageList, setMessageList }) => {
-    const scrollRef = useRef(null);
-    const [chatMessage, setChatMessage] = useState('');
-    const socket = useContext(SocketContext);
+const Chatbox = ({ messageList, setMessageList }) => {
+  const scrollRef = useRef(null);
+  const [chatMessage, setChatMessage] = useState("");
+  const socket = useContext(SocketContext);
+  const [username, setUsername] = useState("Anonymous");
 
-    // always scroll to bottom of text chat when there are new messages
-    useEffect(() => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollIntoView({ behaviour: "smooth" });
-        }
-    }, [messageList]);
+  useEffect(() => {
+    axios
+      .get("/user")
+      .then((res) => {
+        setUsername(res.data.username);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-    return (
-        <Card >
-            <Card.Header title="MessageBoard" />
-            <Card.Body style={{"overflow-y": "scroll" , "height": "75vh"}} >
-                <ListGroup>
-                    {messageList.map((message, index) => {
-                        return (
-                            <ListGroup.Item key={index}>{message}</ListGroup.Item>
-                        );
-                    })}
-                    <ListGroup.Item ref={scrollRef} />
-                </ListGroup>
-            </Card.Body>
-            <Card.Footer >
-                <InputGroup className="mb-3">
-                    <FormControl
-                        placeholder="Send a message"
-                        aria-label="Send a message"
-                        aria-describedby="basic-addon2"
-                        value={chatMessage}
-                        onChange={(event) => {
-                            setChatMessage(event.target.value);
-                        }}
-                        onKeyPress={(e) => {
-                            if (e.key === 'Enter' && chatMessage) {
-                                socket.emit('message', {
-                                    username: "test-username",
-                                    message: chatMessage
-                                });
-                                setChatMessage('');
-                            }
-                        }}
-                    />
-                    <InputGroup.Append>
-                        <Button
-                            variant="outline-secondary"
-                            onClick={() => {
-                                if (chatMessage) {
-                                    socket.emit('message', {
-                                        username: "test-username",
-                                        message: chatMessage    
-                                    });
-                                    setChatMessage('');
-                                }
-                            }}>Send</Button>
-                    </InputGroup.Append>
-                </InputGroup>
-            </Card.Footer>
-        </Card>
-    );
+  // always scroll to bottom of text chat when there are new messages
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behaviour: "smooth" });
+    }
+  }, [messageList]);
+
+  return (
+    <Card>
+      <Card.Header title="MessageBoard" />
+      <Card.Body style={{ "overflow-y": "scroll", height: "75vh" }}>
+        <ListGroup>
+          {messageList.map((message, index) => {
+            return <ListGroup.Item key={index}>{message}</ListGroup.Item>;
+          })}
+          <ListGroup.Item ref={scrollRef} />
+        </ListGroup>
+      </Card.Body>
+      <Card.Footer>
+        <InputGroup className="mb-3">
+          <FormControl
+            placeholder="Send a message"
+            aria-label="Send a message"
+            aria-describedby="basic-addon2"
+            value={chatMessage}
+            onChange={(event) => {
+              setChatMessage(event.target.value);
+            }}
+            onKeyPress={(e) => {
+              if (e.key === "Enter" && chatMessage) {
+                socket.emit("message", {
+                  username: username,
+                  message: chatMessage,
+                });
+                setChatMessage("");
+              }
+            }}
+          />
+          <InputGroup.Append>
+            <Button
+              variant="outline-secondary"
+              onClick={() => {
+                if (chatMessage) {
+                  socket.emit("message", {
+                    username: username,
+                    message: chatMessage,
+                  });
+                  setChatMessage("");
+                }
+              }}
+            >
+              Send
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </Card.Footer>
+    </Card>
+  );
 };
 
 export default Chatbox;
